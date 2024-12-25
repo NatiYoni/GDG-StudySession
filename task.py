2369346df61fc1d89a4eb4fd0a27de9919629cb5
Natnael Yonas <yonasnatnael11@gmial.com>
2024-12-25 12:49:18 +0300

    completed question 2: Print Even Numbers Between 1 and 20

--- task.py
+++ task.py
@@ -3,19 +3,3 @@ def sum_numbers(numbers):
     return sum(numbers)
 
 print(sum_numbers([1, 2, 3, 4, 5]))
-
-# Question 3: Find the Largest Number in a List
-def find_largest(numbers):
-    return max(numbers)
-
-print(find_largest([3, 5, 7, 2, 8]))
-
-# Question 4: Count Word Frequency in a Sentence
-def word_frequency(sentence):
-    words = sentence.split()
-    freq = {}
-    for word in words:
-        freq[word] = freq.get(word, 0) + 1
-    return freq
-
-print(word_frequency("this is a test this is only a
