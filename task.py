f3fb54fa977898da394c48ba3310841ced4e0bb8
Natnael Yonas <yonasnatnael11@gmial.com>
2024-12-25 12:49:18 +0300

    completed question 4: Count Word Frequency in a Sentence

--- task.py
+++ task.py
@@ -1,15 +1,3 @@
-# Question 1: Sum Numbers in a List
-def sum_numbers(numbers):
-    return sum(numbers)
-
-print(sum_numbers([1, 2, 3, 4, 5]))
-
-# Question 3: Find the Largest Number in a List
-def find_largest(numbers):
-    return max(numbers)
-
-print(find_largest([3, 5, 7, 2, 8]))
-
 # Question 4: Count Word Frequency in a Sentence
 def word_frequency(sentence):
     words = sentence.split()
@@ -18,4 +6,5 @@ def word_frequency(sentence):
         freq[word] = freq.get(word, 0) + 1
     return freq
 
-print(word_frequency("this is a test this is only a
+print(word_frequency("this is a test this is only a test"))
+
