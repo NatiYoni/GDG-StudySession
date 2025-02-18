80c596fc851d271a9aa4cf40b1a51292f95b6a0c
Natnael Yonas <148974950+NatiYoni@users.noreply.github.com>
2025-02-18 23:35:51 +0300

    Create main.py

---
+++ month-1/week-3/main.py
@@ -0,0 +1,11 @@
+from car import Car
+from bike import Bike
+
+def vehicle_info(vehicle):
+    print(vehicle.describe())
+
+car = Car("Toyota", "Corolla", 4)
+bike = Bike("Yamaha", "MT-07", "Sport")
+
+vehicle_info(car)
+vehicle_info(bike)
